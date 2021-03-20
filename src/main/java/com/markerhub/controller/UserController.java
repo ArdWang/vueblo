9a1ced56d11fee50962abf6d19e1e46896704eaa
ardwang <278161009@qq.com>
2021-03-20 16:40:09 +0800

    update rnd 2021/3/20

---
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.markerhub.controller;
+
+
+import com.markerhub.common.lang.Result;
+import com.markerhub.entity.User;
+import com.markerhub.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 关注公众号：MarkerHub
+ * @since 2021-03-20
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequiresAuthentication
+    @GetMapping("/index")
+    public Object index(){
+        User user = userService.getById(1L);
+        return Result.success(200,"操作成功！", user);
+    }
+
+}
